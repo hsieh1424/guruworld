f992e05e84c7946e875a4b8dd84e39da51d5a402
hsieh_chiachun1424 <k021g1424@g.neec.ac.jp>
2022-11-05 11:37:36 +0900

    回転おｋ

--- GuruWorld/Assets/change.cs
+++ GuruWorld/Assets/change.cs
@@ -7,6 +7,10 @@ public class change : MonoBehaviour
 {
     public GameObject Ground;
     public GameObject Player;
+    public GameObject Up;
+    public GameObject Down;
+    public GameObject Right;
+    public GameObject Left;
     public int changeflag = 0;
     public int changenum = 0;
     public int delay = 120;
@@ -14,57 +18,38 @@ public class change : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        changeflag = 0;
+        GameFlag.changeflag = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        changeflag = GameFlag.changeflag;
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Right")
-        {
-            changeflag = 1;
-        }
-        else if (other.gameObject.tag == "Left")
-        {
-            changeflag=2;
-        }
-        else if (other.gameObject.tag == "Up")
-        {
-            changeflag = 3;
-        }
-        else if (other.gameObject.tag == "Down")
-        {
-            changeflag = 4;
-        }
-    }
+   
 
         private void OnTriggerEnter(Collider other)
     {
-
-            if (other.gameObject.tag == "Player"&& changeflag==1)
+            if (other.gameObject.tag == "Player"&& GameFlag.changeflag == 2)
             {
                 Debug.Log("in1");
                 Ground.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, -1, 0), 90);
                 Player.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, -1, 0), 90);
             }
-            else if (other.gameObject.tag == "Player" && changeflag ==2)
+            else if (other.gameObject.tag == "Player" && GameFlag.changeflag == 1)
             {
                 Debug.Log("in2");
                 Ground.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 1, 0), 90);
                 Player.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 1, 0), 90);
             }
-            else if (other.gameObject.tag == "Player" && changeflag ==3)
+            else if (other.gameObject.tag == "Player" && GameFlag.changeflag == 3)
             {
                 Debug.Log("in3");
                 Ground.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 0, -1), 90);
                 Player.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 0, -1), 90);
             }
-            else if (other.gameObject.tag == "Player" && changeflag ==4)
+            else if (other.gameObject.tag == "Player" && GameFlag.changeflag == 4)
             {
                 Debug.Log("in4");
                 Ground.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 0, 1), 90);
@@ -115,9 +100,9 @@ public class change : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (changeflag != 0)
+        if (GameFlag.changeflag != 0)
         {
-            changeflag = 0;
+            GameFlag.changeflag = 0;
             Debug.Log("out");
         }
     }
